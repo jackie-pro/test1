d90195e3e58652d64398067151864a70eb1ecd43
jackie-pro <65387993+jackie-pro@users.noreply.github.com>
2020-05-19 14:29:55 +0800

    first commit

---
+++ git-test.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+a=eval(input("請輸入矩形的長:"))
+b=eval(input("請輸入矩形的寬:"))
+print("矩形的周長為:",int(2*a+2*b),"矩形的面積為:",int(a*b))
+
+
+#輸入任意2個數值a,b 得出a的b次方值
+def index(a,b):
+    x=a**b
+    print("a的b次方為",x)
+d=eval(input("請輸入一個值\"a\":"))
+e=eval(input("請輸入次方數\"b\":"))
+index(d,e)
+
+a=eval(input("請輸入矩形的長:"))
+b=eval(input("請輸入矩形的寬:"))
+print("矩形的周長為:",int(2*a+2*b),"矩形的面積為:",int(a*b))
+print("矩形的周長為:",int(2*a+2*b),"矩形的面積為:",int(a*b))
+a=eval(input("請輸入矩形的長:"))
+b=eval(input("請輸入矩形的寬:"))
+print("矩形的周長為:",int(2*a+2*b),"矩形的面積為:",int(a*b))
